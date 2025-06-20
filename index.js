dc88f6083c71f80b9de6902c2ab042cd399187f1
Jesse <jessejamesjj007@gmail.com>
2025-06-20 21:25:53 +0300

    Add index.js content

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(userName, userEmail){
+    
+    let details = {
+    name: userName,
+    email: userEmail,
+    }
+
+    const userDetails = {
+        method : 'POST',
+        headers :{
+            "Content-Type" : 'application/json',
+            "Accept" : "application/json"
+        },
+        body : JSON.stringify(details), 
+    }     
+
+    return fetch ("http://localhost:3000/users" , userDetails)
+    .then(resp => resp.json())
+    .then(userData => console.log(userData))
+}
+document.getElementById("submit").addEventListener("click", (e) => {
+  e.preventDefault()  
+  submitData("Kelly", "kelly@gmail.com");
+});
+
+
+
+
+
+
+
